09f8815f96c1ab842601d30e93cd9312496c5810
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-07-03 12:16:11 -0300

    primevue setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,8 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -13,6 +15,12 @@ createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
+            .use(PrimeVue, {
+                // unstyled: true
+                theme: {
+                    preset: Aura
+                }
+            })
             .use(plugin)
             .use(ZiggyVue)
             .mount(el);
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import {PrimeVueResolver} from '@primevue/auto-import-resolver';
 
 export default defineConfig({
     plugins: [
@@ -15,6 +17,11 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
+        }),
+        Components({
+            resolvers: [
+                PrimeVueResolver()
+            ]
         }),
     ],
 });
