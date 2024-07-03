c859952e172712dc19356a91d974f92c89d71c8c
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-07-03 14:16:08 -0300

    feedback component for inputs

---
+++ resources/js/Components/InputFeedBack.vue
@@ -0,0 +1,27 @@
+<template>
+    <div :class="['mb-2', containerClass]">
+        <small v-if="errorText!=null" :id="`${inputId}-error`" class="text-red-600 dark:text-red-400">{{ errorText }}</small>
+        <small v-else v-show="helpText!=null" :id="`${inputId}-help`" class="text-gray-600 dark:text-gray-400">{{ helpText }}</small>
+        <hr v-show="errorText!=null||helpText!=null">
+   </div>
+</template>
+<script setup>
+
+const props = defineProps({
+    inputId: {
+        type: String,
+    },
+    helpText: {
+        type: String,
+        default: null
+    },
+    errorText: {
+        type: String,
+        default: null,
+    },
+    containerClass: {
+        type: String,
+        default: null,
+    },
+})
+</script>
