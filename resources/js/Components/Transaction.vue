8c7223fe1e4ec15212275370a507f481fd766593
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-07-08 12:44:34 -0300

    transaction view refact

--- resources/js/Components/Transaction.vue
+++ resources/js/Components/Transaction.vue
@@ -8,20 +8,31 @@ defineProps([
 ])
 </script>
 <template>
-    <div class="p-4 flex space-x-2">
-        <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-400" />
+    <div class="p-4 flex space-x-3">
+        <ApplicationLogo class="h-9 w-auto text-gray-800 dark:text-gray-400" />
         <div class="flex-1">
-            <p class="text-lg text-gray-800">{{ data.quantity }}</p>
-            <div class="flex space-x-4">
-                <span class="text-sm text-gray-800">De:</span>
-                <span class="text-sm text-gray-800">{{ data.from_storage.owner_user.name }} - <span class="text-sm text-gray-400">{{ data.from_storage.name }}</span></span>
-                <span class="text-sm text-gray-800">Para:</span>
-                <span class="text-sm text-gray-800">{{ data.to_storage.owner_user.name }} - <span class="text-sm text-gray-400">{{ data.to_storage.name }}</span></span>
+            <h4 class="text-lg text-gray-900 w-full">Transferência n°: {{ data.id }}</h4>
+            <p class="text-lg text-gray-900 w-full">{{ data.name }}</p>
+            <div class="flex flex-row-2 gap-1">
+                <Fieldset legend="Origem" class="w-full">
+                    <div class="text-sm text-gray-500 flex flex-col">
+                        <span class="text-gray-700">{{ data.from_storage.owner_user.name }}</span>
+                        <span class="text-gray-500">{{ data.from_storage.name }}</span>
+                    </div>
+                </Fieldset>
+                <Fieldset legend="Destino" class="w-full">
+                    <div class="text-sm text-gray-500 flex flex-col">
+                        <span class="text-gray-700">{{ data.to_storage.owner_user.name }}</span>
+                        <span class="text-gray-500">{{ data.to_storage.name }}</span>
+                    </div>
+                </Fieldset>
             </div>
+            <p class="text-sm text-gray-400">Informações: {{ data.description }}</p>
             <div class="flex flex-row-reverse">
                 <span class="text-sm text-gray-800">{{ data.creator_user.name }}</span>
                 <small class="mr-2 text-sm text-gray-600">{{ dayjs(data.created_at).fromNow() }}</small>
             </div>
         </div>
+        <p class="text-4xl text-gray-800 content-center">{{ data.quantity }}</p>
     </div>
 </template>
