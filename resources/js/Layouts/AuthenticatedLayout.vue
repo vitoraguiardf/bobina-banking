151ccb5731f708856e41f947fec6ba88730d38f4
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-07-12 16:30:38 -0300

    add toast service

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -12,7 +12,8 @@ const showingNavigationDropdown = ref(false);
 
 <template>
     <div>
-        <ConfirmDialog></ConfirmDialog>
+        <Toast />
+        <ConfirmDialog />
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             <nav class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
                 <!-- Primary Navigation Menu -->
--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
+import ToastService from 'primevue/toastservice'
 import ConfirmationService from 'primevue/confirmationservice';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
@@ -23,6 +24,7 @@ createInertiaApp({
                 }
             })
             .use(ConfirmationService)
+            .use(ToastService)
             .use(plugin)
             .use(ZiggyVue)
             .mount(el);
