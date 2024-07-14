509a73e159315b27175789a941681dd712c7835f
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-07-14 17:26:52 -0300

    add created component

---
+++ resources/js/Components/Created.vue
@@ -0,0 +1,24 @@
+<script setup>
+import relativeTime from 'dayjs/plugin/relativeTime';
+import dayjs from 'dayjs';
+dayjs.extend(relativeTime)
+defineProps([
+    'data'
+])
+</script>
+<template>
+    <div class="flex-row text-sm text-gray-600 dark:text-gray-400">
+        <small class="flex-none" v-if="data.creator_user!=null">
+            <span :title="`Creator`">{{ data.creator_user.name }}</span>
+        </small>
+        <small class="flex-none">
+            &middot;
+            <span :title="`Created at ${dayjs(data.created_at).fromNow()}`"
+                >{{ dayjs(data.created_at).fromNow() }}</span>
+        </small>
+        <small class="flex-none" v-if="data.created_at != data.updated_at">
+            &middot;
+            <span :title="`Updated at ${dayjs(data.updated_at).fromNow()}`">edited</span>
+        </small>
+    </div>
+</template>
