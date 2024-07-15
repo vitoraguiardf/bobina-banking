e5552b0f1a9a605a989ec13362bec1ef9f84b851
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-07-15 10:37:34 -0300

    trait for coil-storages

--- app/Models/Office.php
+++ app/Models/Office.php
@@ -2,23 +2,19 @@
 
 namespace App\Models;
 
+use App\Models\traits\CoilStorages\WithCoilStorages;
 use App\Models\traits\Transactions\WithFrom;
 use App\Models\traits\Transactions\WithTo;
 use App\Models\traits\WithCreator;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Office extends Model
 {
-    use HasFactory, WithCreator, WithFrom, WithTo;
+    use HasFactory, WithCreator, WithFrom, WithTo, WithCoilStorages;
     protected $fillable = [
         'creator_user_id',
         'description',
         'name',
     ];
-
-    function coilStorages(): MorphMany {
-        return $this->morphMany(CoilStorage::class, 'holder');
-    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\traits\CoilStorages\WithCoilStorages;
 use App\Models\traits\Transactions\WithFrom;
 use App\Models\traits\Transactions\WithTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -12,7 +13,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, WithFrom, WithTo;
+    use HasFactory, Notifiable, WithFrom, WithTo, WithCoilStorages;
 
     /**
      * The attributes that are mass assignable.
