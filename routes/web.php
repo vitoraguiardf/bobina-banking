b7ad5f6bd9dd2a08b7c027621d06b5cb9472edca
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-07-19 14:25:12 -0300

    route adjusts

--- routes/web.php
+++ routes/web.php
@@ -10,43 +10,34 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
+Route::get('/', fn() => Inertia::render('Welcome', [
+    'canAccessControl' => Route::has('access-control.dashboard'),
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    'phpVersion' => PHP_VERSION
+]));
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
+    Route::middleware('verified')->group(function () {
+        Route::get('/dashboard', fn () => Inertia::render('Dashboard'))
+            ->name('dashboard');
 Route::resource('users', UserController::class)
-    ->only(['index'])
-    ->middleware(['auth', 'verified']);
-
+            ->only(['index']);
 Route::resource('office', OfficeController::class)
-    ->only(['index', 'create', 'store', 'destroy'])
-    ->middleware(['auth', 'verified']);
-
+            ->only(['index', 'create', 'store', 'destroy']);
 Route::resource('coil-storage', CoilStorageController::class)
-    ->only(['index', 'create', 'store', 'destroy'])
-    ->middleware(['auth', 'verified']);
-
+            ->only(['index', 'create', 'store', 'destroy']);
 Route::resource('transaction-types', TransactionTypeController::class)
-    ->only(['index', 'store', 'create', 'destroy'])
-    ->middleware(['auth', 'verified']);
-
+            ->only(['index', 'store', 'create', 'destroy']);
 Route::resource('transaction', TransactionController::class)
-    ->only(['index', 'store', 'update', 'destroy', 'create'])
-    ->middleware(['auth', 'verified']);
+            ->only(['index', 'store', 'update', 'destroy', 'create']);
+    });
+    
+});
 
 require __DIR__.'/auth.php';
