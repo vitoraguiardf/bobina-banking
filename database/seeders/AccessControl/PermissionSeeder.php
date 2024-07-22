8efe24cdcb91dce4cb91052c93d90eb21fe54637
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-07-22 12:16:13 -0300

    setup role and permission seeders

---
+++ database/seeders/AccessControl/PermissionSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders\AccessControl;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Database\Seeders\AccessControl\PermissionSeeder;
+use Database\Seeders\AccessControl\RoleSeeder;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -19,5 +21,11 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        // Seeding default permissions
+        $this->call(PermissionSeeder::class);
+        // Seeding default roles
+        $this->call(RoleSeeder::class);
+
     }
 }
