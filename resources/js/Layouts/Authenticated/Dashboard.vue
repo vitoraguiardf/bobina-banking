5c388dc5eed4678e3c832dc7cfa2d8df2f51a2ea
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-07-23 14:43:29 -0300

    refact dashboards

--- resources/js/Layouts/Authenticated/Dashboard.vue
+++ resources/js/Layouts/Authenticated/Dashboard.vue
@@ -3,33 +3,21 @@ import NavBar from '@/Components/Layout/Nav/Bar.vue';
 import Heading from '@/Components/Layout/Page/Heading.vue';
 const navOptions = {
     home: {
-        name: 'Access Control',
-        route: route('access-control.dashboard'),
-        active: route().current('access-control.dashboard'),
+        name: 'Dashboard',
+        route: route('dashboard'),
+        active: route().current('dashboard'),
         hasAccess: true,
     },
     navItems: [
         {
-            name: 'Users',
-            route: route('access-control.users.index'),
-            active: route().current('access-control.users.index'),
-            hasAccess: true,
-        },
-        {
-            name: 'Roles',
-            route: route('access-control.roles.index'),
-            active: route().current('access-control.roles.index'),
-            hasAccess: true,
-        },
-        {
-            name: 'Permissions',
-            route: route('access-control.permissions.index'),
-            active: route().current('access-control.permissions.index'),
+            name: 'Dashboard',
+            route: route('dashboard'),
+            active: route().current('dashboard'),
             hasAccess: true,
         },
     ],
     profileItems: [
-        {
+    {
             name: 'Home',
             route: route('dashboard'),
             active: route().current('dashboard'),
@@ -54,7 +42,7 @@ const navOptions = {
     <div>
         <Toast />
         <ConfirmDialog />
-        <div class="min-h-screen bg-red-100 dark:bg-red-900">
+        <div class="min-h-screen bg-green-100 dark:bg-green-900">
 
             <!-- Navigation Bar -->
             <NavBar :options="navOptions" />
