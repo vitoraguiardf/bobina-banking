51750ee261381f0a62a14d02c9465fa4aad188ea
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-07-26 12:08:56 -0300

    bobina-banking seeders

---
+++ database/seeders/BobinaBankingTestsSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Seeders;
+
+class BobinaBankingTestsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
