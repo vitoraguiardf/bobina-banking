ee1947c402a37d284d2754b79ad971eab7861470
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-07-31 13:13:01 -0300

    fix routes

--- routes/access-control.php
+++ routes/access-control.php
@@ -11,18 +11,19 @@ Route::group([
     'prefix' => 'access-control',
     'middleware' => ['auth', 'web', 'auth:web', 'verified', 'role:office.admin'],
 ], function () {
-    Route::middleware('permission:access-control.dashboard')->get('/', fn() => Inertia::render('AccessControl/Dashboard'))->name('dashboard');
-    customRoute('permission:access-control', 'users', UserController::class, ['index']);
-    customRoute('permission:access-control', 'roles', RoleController::class, ['index', 'create', 'store', 'destroy']);
-    customRoute('permission:access-control', 'permissions', PermissionController::class, ['index']);
+    Route::middleware('permission:access-control.dashboard')
+        ->get('/', fn() => Inertia::render('AccessControl/Dashboard'))
+        ->name('dashboard');
+    
+    foreach (['index'] as $method)
+        Route::middleware("permission:access-control.users.$method")
+            ->resource('users', UserController::class)->only($method);
+    
+    foreach (['index', 'create', 'store', 'destroy'] as $method)
+        Route::middleware("permission:access-control.roles.$method")
+            ->resource('roles', RoleController::class)->only($method);
+    
+    foreach (['index'] as $method)
+        Route::middleware("permission:access-control.permissions.$method")
+            ->resource('permissions', PermissionController::class)->only($method);
 });
-
-function customRoute(
-    string $permissionPrefix,
-    string $name, string $controller,
-    array $methods): void {
-    foreach ($methods as $method) {
-        Route::middleware("$permissionPrefix.$name.$method")
-            ->resource($name, $controller)->only($method);
-    }
-}
